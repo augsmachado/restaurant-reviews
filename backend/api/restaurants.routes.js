6d0f45c9bb0e68e6cd004773b702197bf841f326
Augs Machado <augustomachadoramos@gmail.com>
2021-09-11 18:10:29 -0300

    [backend | dao] Create POST, PUT and DELETE methods to reviews.controller.js

--- backend/api/restaurants.routes.js
+++ backend/api/restaurants.routes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import RestaurantsCtrl from "./restaurants.controller.js"
+import RestaurantsCtrl from "./restaurants.controller.js";
+import ReviewsCtrl from "./reviews.controller.js";
 
 const router = express.Router();
 
 // Define application routes
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
+
+router
+	.route("/review")
+	.post(ReviewsCtrl.apiPostReview)
+	.put(ReviewsCtrl.apiUpdateReview)
+	.delete(ReviewsCtrl.apiDeleteReview);
 
 export default router;
