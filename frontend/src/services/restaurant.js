7d4d7aa4f12589e9099ff284186ce6993340a828
Augs Machado <augustomachadoramos@gmail.com>
2021-09-12 23:28:29 -0300

    [frontend | components] FIX: restaurant-list.js display

--- frontend/src/services/restaurant.js
+++ frontend/src/services/restaurant.js
@@ -1,33 +1,33 @@
-import http from "../http-common";
+import http from "../http-common.js";
 
 class RestaurantDataService {
-    getAll(page = 0) {
-        return http.get(`?page=${page}`);
-    }
+	getAll(page = 0) {
+		return http.get(`?page=${page}`);
+	}
 
-    get(id) {
-        return http.get(`/id/${id}`);
-    }
+	get(id) {
+		return http.get(`/id/${id}`);
+	}
 
-    find(query, by = "name", page = 0) {
-        return http.get(`?${by}=${query}&page=${page}`);
-    }
+	find(query, by = "name", page = 0) {
+		return http.get(`?${by}=${query}&page=${page}`);
+	}
 
-    createReview(data) {
-        return http.post("/review", data);
-    }
+	createReview(data) {
+		return http.post("/review", data);
+	}
 
-    updateReview(data) {
-        return http.put("/review", data);
-    }
+	updateReview(data) {
+		return http.put("/review", data);
+	}
 
-    deleteReview(id) {
-        return http.delete(`/review?id=${id}`);
-    }
+	deleteReview(id) {
+		return http.delete(`/review?id=${id}`);
+	}
 
-    getCuisines(id) {
-        return http.get(`/cuisines`);
-    }
+	getCuisines(id) {
+		return http.get(`/cuisines`);
+	}
 }
 
 export default new RestaurantDataService();
